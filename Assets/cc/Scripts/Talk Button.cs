03ecde9de864605d00e519a63de5fc9e7c410833
nanafish28 <nina79500@gmail.com>
2024-11-16 23:35:22 +0800

    1116

--- Assets/cc/Scripts/Talk Button.cs
+++ Assets/cc/Scripts/Talk Button.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class TalkButton : MonoBehaviour
 {
-    public GameObject Button;
-    public GameObject talkUI;
+    public GameObject Button; // 設定按鈕物件
+    public GameObject talkUI; // 設定對話框 UI
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // 當觸發時啟動 Button
-        Button.SetActive(true);
+        if (Button != null) // 檢查是否已經指派 Button
+        {
+            Button.SetActive(true);
+        }
+        else
+        {
+            UnityEngine.Debug.LogError("Button has not been assigned in the Inspector!");
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        // 當退出觸發區域時停用 Button
-        Button.SetActive(false);
+        if (Button != null) // 檢查是否已經指派 Button
+        {
+            Button.SetActive(false);
+        }
     }
 
     private void Update()
     {
-        if (Button.activeSelf && Input.GetKeyDown(KeyCode.R))
+        if (Button != null && Button.activeSelf && Input.GetKeyDown(KeyCode.R))
         {
-            talkUI.SetActive(true);
+            if (talkUI != null) // 檢查是否已經指派 talkUI
+            {
+                talkUI.SetActive(true);
+            }
+            else
+            {
+                UnityEngine.Debug.LogError("talkUI has not been assigned in the Inspector!");
+            }
         }
     }
 }
